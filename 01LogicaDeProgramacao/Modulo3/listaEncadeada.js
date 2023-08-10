103de08f046e9f7719519aa794b2aad81276a5da
absoll <uzumakijunior@gmail.com>
2023-08-10 19:41:27 +0000

    Lista encadeada com variavel finalizada. Falta finalizar usando classe

---
+++ 01LogicaDeProgramacao/Modulo3/listaEncadeada.js
@@ -0,0 +1,131 @@
+/*
+Crie uma lista encadeada em que cada elemento representa uma pessoa.
+
+Ela precisa conter informações como nome, idade e referência ao filho dela.
+*/
+
+//argument by value
+//https://stackoverflow.com/questions/1009911/javascript-get-argument-value-and-name-of-passed-variable
+
+
+//lista encadeada sem Class, usando apenas variaveis
+
+let pessoa = {
+    nome: 'nome',
+    idade: 20,
+    proximo: null
+}
+let cabeca = null
+
+// tutorials - shallow and deep cloning
+// https://www.digitalocean.com/community/tutorials/copying-objects-in-javascript
+// https://stackoverflow.com/questions/67882939/how-to-copy-object-to-new-variable
+// https://www.javascripttutorial.net/object/3-ways-to-copy-objects-in-javascript/
+//
+// funcao para criar nova instancia, parecido com "new Class". Isso evita clonar os resultados (ler textos acima)
+function criarNovaInstancia(pessoa){
+    let novaInstancia = JSON.parse(JSON.stringify(pessoa))
+    return novaInstancia
+}
+
+// criando variaveis distintas para serem adicionados como "filhos"
+p1 = criarNovaInstancia(pessoa)
+
+p2 = criarNovaInstancia(pessoa)
+p2['nome'] = 'nome2'
+p2['idade'] = 30
+
+p3 = criarNovaInstancia(pessoa)
+p3['nome'] = "nome3"
+p3['idade'] = 35
+
+// função para determinar quem vai ser a "cabeca" da lista
+function determinarCabeca(pessoa){
+    cabeca = pessoa
+}
+
+// função para alocar um filho na extremidade da lista
+function alocarFilho(filho){
+    // se a cabeça estiver vazia, retorna "falso" para indicar erro
+    if (cabeca == null){ 
+        return false
+    }
+    else{
+        //uma variavel para armazenar o "nó" atual e avançar procurando seu filho
+        let temporariaCabeca = cabeca
+        while(temporariaCabeca['proximo'] != null){
+            temporariaCabeca = temporariaCabeca['proximo']
+        }
+        // aloca o novo "filho" na extremidade da lista
+        temporariaCabeca['proximo'] = filho
+        return true
+    }
+}
+
+/*
+determinarCabeca(p1)
+console.log(alocarFilho(p2))
+console.log(alocarFilho(p3))
+console.log(cabeca)
+*/
+
+
+// lista encadeada usando classe
+
+class Pessoa{
+    constructor(nome, idade){
+        this.nome = nome
+        this.idade = idade
+        this.proximo = null
+    }
+}
+
+p1 = new Pessoa('nome', 20)
+p2 = new Pessoa('nome2', 25)
+p3 = new Pessoa('nome3', 30)
+
+
+class listaEncadeada{
+    constructor(cabeca = null){
+        this.cabeca = cabeca
+    }
+
+    escolherCabeca(cabeca){
+        this.cabeca = cabeca
+    }
+
+    adicionarNo(no){
+        if(this.cabeca == null)
+            return false
+        else{
+            let temporariaCabeca = this.cabeca
+
+            while(temporariaCabeca.proximo != null)
+                temporariaCabeca = temporariaCabeca.proximo
+
+            temporariaCabeca.proximo = no
+            return true
+        }
+    }
+}
+
+lista = new listaEncadeada()
+lista.escolherCabeca(p1)
+console.log(lista.adicionarNo(p2))
+console.log(lista.adicionarNo(p3))
+
+console.log(lista.cabeca)
+
+
+
+/*
+function criarNovaInstancia(pessoa, ...vetor){
+    let novaInstancia = JSON.parse(JSON.stringify(pessoa))
+    console.log(vetor)
+    console.log(vetor.length)
+}
+
+criarNovaInstancia(pessoa, {a: 5}, {b: 7})
+
+*/
+
--- testes.js
+++ testes.js
@@ -26,7 +26,8 @@ else if(idade >= 16 && idade < 18)
 else
     console.log("Usuario não possui permissão para dirigir")
     
-    
+
+//--------------------------------------------------------------------------------
 //Observação para a (idade >= 18)    
 switch(idade) {
     case (idade >= 18): //Nao funciona esta errado.  Switch apenas funciona com "fall-Through", como mostrado com 16 e 17
@@ -41,4 +42,33 @@ switch(idade) {
 }
 
 
+//----------------------------------------------------------------------------------------------
+var x = {
+    'nome': 1,
+    'proximo': null
+}
 
+
+// tutorials - shallow and deep cloning
+// https://www.digitalocean.com/community/tutorials/copying-objects-in-javascript
+// https://stackoverflow.com/questions/67882939/how-to-copy-object-to-new-variable
+// https://www.javascripttutorial.net/object/3-ways-to-copy-objects-in-javascript/
+
+function teste(a, y){
+    let c = JSON.parse(JSON.stringify(a))
+    console.log(c)
+    c['nome'] = y
+    return c
+}
+
+v = [teste(x,2),teste(x,5)]
+
+console.log(v)
+
+
+//----------------------------------------------------------------------------------------
+//Informação extra
+//caso precise saber o nome de uma variavel no programa, digitar "Object.keys({NOME})[0]" que o nome dela é mostrado
+let teste = 5
+
+console.log(Object.keys({teste})[0])
