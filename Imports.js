155634615d8dff7379ddab5a2e74e4941781f22d
absoll <uzumakijunior@gmail.com>
2023-09-15 14:12:46 +0000

    exemplo de import do diretorio raiz

---
+++ Imports.js
@@ -0,0 +1,16 @@
+//Para importar do diretorio raiz, seguir o passo a seguir
+// https://archive.is/20230321224040/https://marian-caikovski.medium.com/how-to-import-a-module-from-the-root-of-a-node-js-project-edb3421534e3
+
+
+
+/*
+dento do "package.json" escrever as seguintes informações:
+
+ "imports": {
+    "#root/*": {
+      "default": "./*"
+    }
+  }
+
+a partir dai, qualquer import que queira iniciar a partir do diretorio raiz vai ser reconhecido como "#root/"
+*/
