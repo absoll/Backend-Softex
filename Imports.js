ec62ed5b60986df65b238ba76f9dc7fcc3804243
absoll <uzumakijunior@gmail.com>
2023-10-12 03:41:42 +0000

    Import.JS melhor explicado

--- Imports.js
+++ Imports.js
@@ -1,6 +1,6 @@
 //Para importar do diretorio raiz, seguir o passo a seguir
 // https://archive.is/20230321224040/https://marian-caikovski.medium.com/how-to-import-a-module-from-the-root-of-a-node-js-project-edb3421534e3
-
+// https://github.com/marianc000/importFromRoot
 
 
 /*
